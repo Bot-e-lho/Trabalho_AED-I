a0ddee8c1af145d3cd0351801187e92603c20f5f
Miguel Botelho <135281223+Bot-e-lho@users.noreply.github.com>
2023-08-15 18:36:36 -0300

    Create matrix.h

---
+++ matrix.h
@@ -0,0 +1,22 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+typedef struct matrix {
+    struct matrix* right;
+    struct matrix* below;
+    int line;
+    int column;
+    float info;
+}Matrix;
+
+
+Matrix* matrix_create(void);
+void matrix_destroy(Matrix* m);
+void matrix_print(Matrix* m);
+Matrix* matrix_add(Matrix* m, Matrix* n);
+Matrix* matrix_multiply(Matrix* m, Matrix* n);
+Matrix* matrix_transpose(Matrix* m);
+float matrix_getelem(Matrix* m, int x, int y);
+void matrix_setelem(Matrix* m, int x, int y, float elem);
+
+#endif /*MATRIX_H*/
