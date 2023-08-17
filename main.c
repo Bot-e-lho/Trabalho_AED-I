aeb992c2c231dd3df516ca054cb3f4f843e4fc1d
Miguel Botelho <135281223+Bot-e-lho@users.noreply.github.com>
2023-08-17 16:57:38 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "matrix.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( void ) {
+/* Inicializacao da aplicacao ... */
+Matrix *A = matrix_create();
+matrix_print( A );
+Matrix *B = matrix_create(); 
+matrix_print( B );
+Matrix *C = matrix_add( A, B ); 
+matrix_print( C );
+matrix_destroy( C );
+C = matrix_multiply( A, B ); 
+matrix_print( C );
+matrix_destroy( C );
+C = matrix_transpose( A ); 
+matrix_print( C );
+matrix_destroy( C );
+matrix_destroy( A );
+matrix_destroy( B );
+return 0;
+}
