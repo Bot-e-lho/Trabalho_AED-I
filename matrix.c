ff381683ff603b433898ae0bb66c4d9c126b810f
Marcos Lima Alves <103121917+limrmarcos@users.noreply.github.com>
2023-08-18 14:51:17 -0300

    Update matrix.c
    Alterações feitas em aula, ajustes no create e adição de duas funções para ajudar na criação das cabeças

--- matrix.c
+++ matrix.c
@@ -3,12 +3,6 @@
 #include <stdlib.h>
 
 Matrix* matrix_create( void ) {
-/* A ideia inicial era fazer aquela cabeça a mais aqui e colocar as outras a partir
-de uma função externa (matrix_help), porém isso parece meio estranho.*/
-
-
-/*Tem a opção de transformar em um vetor de matrizes e usar o indice para 
-facilitar na hora de adicionar elementos, isso também parece estranho*/
   Matrix* matrix = ( Matrix* ) malloc ( sizeof( Matrix ) );
 
   matrix->line = -1;
@@ -22,7 +16,11 @@ facilitar na hora de adicionar elementos, isso também parece estranho*/
 
   scanf( "%d %d", &m, &n );
   for( int a = 0; a < m; a++ ) {
-    matrix_help( matrix, a );
+    matrix_heads_line( matrix, a );
+  }
+
+  for( int b = 0; b < n; b++ ) {
+    matrix_heads_column( matrix, b );
   }
 
   do {
@@ -30,41 +28,37 @@ facilitar na hora de adicionar elementos, isso também parece estranho*/
     if( valor != 0 ) {
     matrix_setele( matrix, i, j, valor );
     }
-  } while( valor != 0 || valor != '.' );
+  } while( valor != 0 );
 }
 
-void matrix_help( Matrix* m, int a ) {
-  //fiz na ideia de criar as outras caixas, olhando melhor parece meio exagerada.
-  Matrix* new_right = ( Matrix* ) malloc ( sizeof( Matrix ) );
-  Matrix* new_below = ( Matrix* ) malloc ( sizeof( Matrix ) );
+void matrix_heads_line( Matrix* m, int a ) {
+  Matrix* new = ( Matrix* ) malloc ( sizeof( Matrix ) );
+  Matrix* aux = m->below;
 
-  new_right->line = -1; // Acho que ta invertido o right com o below
-  new_right->column = -1;
-  m->below = new_right;
-  new_right->below = m;
-
-  new_below->line = -1;
-  new_below->column = -1;
-  m->line = new_below;
-  new_below = m;
-/*
-  new_right->line = -1;
-  new_right->column = -1;
-  new_right->right = m->right;
-  m->right = new_right;
-  
-  new_below->line = -1;
-  new_below->column = -1;
-  new_below->below = m->below;
-  m->below = new_below;
-  
-*/
-
-  free( new_right );
-  free( new_below );
-
-  
+  while( aux->below != m ) {
+    aux = aux->below;
+  }
+  new->line = a;
+  new->column = -1;
+  aux->below = new;
+  new->below = m;
+  new->right = new;
 }
+
+void matrix_heads_column( Matrix* m, int a ) {
+  Matrix* new = ( Matrix* ) malloc ( sizeof( Matrix ) );
+  Matrix* aux = m->right;
+
+  while( aux->right != m ) {
+    aux = aux->right;
+  }
+  new->line = -1;
+  new->column = a;
+  aux->right = new;
+  new->right = m;
+  new->below = new;
+}
+
 //Pesquisei como percorrer uma matriz encadeada circular
 //utilizei a mesma logica para o print
 //não sei se é do jeito que imaginavas
@@ -138,20 +132,21 @@ Matrix* matrix_add(Matrix* m, Matrix* n) {
             temp->info = aux_m->info + aux_n->info;
             temp->right = temp;
             temp->below = temp;
-          }
-          if ( aux_temp == NULL ) {
+          
+            if ( aux_temp == NULL ) {
               res->right = temp;
-          } else {
+            } else {
               aux_temp->right = temp;
-          } 
-          aux_temp = temp;
+            } 
+            aux_temp = temp;
+          }
           aux_m = aux_m->right;
           aux_n = aux_n->right;
     } 
   return res;
 }
 
-Matrix* matrix_multiply(Matrix* m, Matrix* n) {//m - linha e n - coluna (aux)
+Matrix* matrix_multiply( Matrix* m, Matrix* n ) {//m - linha e n - coluna (aux)
   if ( m == NULL || n == NULL || m->column != n->line ) {
       return NULL;
   } 
@@ -163,15 +158,15 @@ Matrix* matrix_multiply(Matrix* m, Matrix* n) {//m - linha e n - coluna (aux)
     while ( aux_m != r_head_m ) {
             Matrix* aux_n = r_head_n->right;
             while ( aux_n != r_head_n ) {
-                    float soma = 0.00;
+                    float sum = 0.00;
                     Matrix* temp_n = aux_n->below;
                     Matrix* temp_m = aux_m->right;
                     while ( temp_m != aux_m && temp_n != aux_n ) {
                       	    if ( temp_m->column != temp_n->line ) {
-				 printf("\nValores de Colunas(m) e Linhas(n) é incopativel...\n");
+				 printf("\nValores de Colunas(m) e Linhas(n) é incompativel...\n");
 				 return NULL;
 			    } else {
-				soma = temp_m->info * temp_n->info;
+				sum = temp_m->info * temp_n->info;
 				temp_m = temp_m->right;
 				temp_n = temp_n->below;
 			    } 
@@ -187,7 +182,7 @@ Matrix* matrix_multiply(Matrix* m, Matrix* n) {//m - linha e n - coluna (aux)
   return res;
 }
 
-Matrix* matrix_transpose(Matrix* m) {
+Matrix* matrix_transpose( Matrix* m ) {
   if ( m == NULL ) {
        return NULL;
   } 
@@ -246,7 +241,7 @@ void matrix_setelem(Matrix* m, int x, int y, float elem) {
 	  temp_2 = temp_2->right;
   } 
   if ( temp_2 != aux && aux->column == y ) {
-       temp_2 = elem;
+       temp_2->info = elem;
   } else { 
 	Matrix* new = ( Matrix* ) malloc ( sizeof( Matrix ) );
   	new->line = x;
@@ -254,9 +249,7 @@ void matrix_setelem(Matrix* m, int x, int y, float elem) {
   	new->info = elem;
 	new->right = temp_2;
 	temp_1->right = new;
-  } 
-  
-
+  }
   //Criando uma caixinha para adicionar o valor, porém também essa área também pode servir apenas
   //para trocar o valor de um nodo já existente.
 }
