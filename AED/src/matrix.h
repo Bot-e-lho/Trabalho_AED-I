379a560286860b2b8fe10b68c28ff549247d5692
Marcos Lima Alves <103121917+limrmarcos@users.noreply.github.com>
2023-08-24 01:59:57 -0300

    Update matrix.h

--- AED/src/matrix.h
+++ AED/src/matrix.h
@@ -120,6 +120,7 @@ void matrix_print( Matrix* m ) {
     while( test->right != m ) {
         test = test->right;
     }
+    printf( "\n" );
 
     Matrix* r_head = m;
     Matrix* aux_row = r_head->below;
@@ -199,26 +200,39 @@ Matrix* matrix_multiply( Matrix* m, Matrix* n ) {
         return NULL;
     }
 
-    int m_linha = m->line;
-    int m_col = m->column;
-    //int n_linha = n->line;
-    int n_col = n->column;
+    Matrix* aux = m->below;
+    while( aux->below != m ) {
+        aux = aux->below;
+    }
+    int line = aux->line + 1;
+    aux = m->right;
+    while( aux->right != m ) {
+        aux = aux->right;
+    }
+    int m_column = aux->column + 1;
 
-    float** res = (float**)malloc(m_linha * sizeof(float*));
-    for( int i = 0; i < m_linha; i++ ) {
-        res[i] = ( float* )malloc( n_col * sizeof( float ) );
-        for( int j = 0; j < n_col; j++ ) {
+    aux = n->right;
+
+    while( aux->right != n ) {
+        aux = aux->right;
+    }
+    int n_column = aux->column + 1;
+
+    float** res = (float**)malloc(line * sizeof(float*));
+    for( int i = 0; i < line; i++ ) {
+        res[i] = ( float* )malloc( n_column * sizeof( float ) );
+        for( int j = 0; j < n_column; j++ ) {
             float value = 0.0;
-            for (int k = 0; k < m_col; k++) {
+            for (int k = 0; k < m_column; k++) {
                 value += matrix_getelem( m, i, k ) * matrix_getelem( n, k, j );
             }
             res[i][j] = value;
         }
     }
 
-    Matrix* matrix = matrix_create_with_values(m_linha, n_col, res);
+    Matrix* matrix = matrix_create_with_values(line, n_column, res);
 
-    for( int i = 0; i < m_linha; i++ ) {
+    for( int i = 0; i < line; i++ ) {
         free( res[i] );
     }
     free( res );
@@ -231,20 +245,28 @@ Matrix* matrix_transpose( Matrix* m ) {
         return NULL;
     }
 
-    int m_linha = m->column;
-    int m_col = m->line;
+    Matrix* aux = m->below;
+    while( aux->below != m ) {
+        aux = aux->below;
+    }
+    int line = aux->line + 1;
+    aux = m->right;
+    while( aux->right != m ) {
+        aux = aux->right;
+    }
+    int column = aux->column + 1;
 
-    float** res = (float**)malloc(m_linha * sizeof(float*));
-    for( int i = 0; i < m_linha; i++ ) {
-        res[i] = ( float* )malloc( m_col * sizeof( float ) );
-        for( int j = 0; j < m_col; j++ ) {
+    float** res = (float**)malloc(line * sizeof(float*));
+    for( int i = 0; i < line; i++ ) {
+        res[i] = ( float* )malloc( column* sizeof( float ) );
+        for( int j = 0; j < column; j++ ) {
             res[i][j] = matrix_getelem( m, j, i );
         }
     }
 
-    Matrix* matrix = matrix_create_with_values(m_linha, m_col, res);
+    Matrix* matrix = matrix_create_with_values(line, column, res);
 
-    for( int i = 0; i < m_linha; i++ ) {
+    for( int i = 0; i < line; i++ ) {
         free( res[i] );
     }
     free( res );
@@ -256,23 +278,23 @@ float matrix_getelem( Matrix* m, int x, int y ) {
     if( m == NULL || x < 0 || y < 0 ) {
 	    return 0.00;
     }
+
     Matrix* r_head = m;
     Matrix* aux = r_head->below;
-  	while( aux != r_head || aux->line != x ) {
-	  	aux = aux->below;
+  	while( aux != r_head && aux->line != x ) {
+        aux = aux->below;
     } 
  	if( aux == r_head ) {
-	    return 0.00;
+	    return 0.0; 
 	} 
 	Matrix* temp = aux->right;
-	while( temp != aux || temp->column != y ) {
-		temp = temp->right;
+	while( temp != aux && temp->column != y ) {
+        temp = temp->right;
 	}
 	if( temp == aux ) {
-	    return 0.00;
+	    return 0.0; 
 	}
-	
-  return temp->info;
+    return temp->info;
 }
 
 void matrix_setelem( Matrix* m, int x, int y, float elem ) {
@@ -302,7 +324,20 @@ void matrix_setelem( Matrix* m, int x, int y, float elem ) {
         new_node->info = elem;
         new_node->right = temp_2;
         temp_1->right = new_node;
+        aux = r_head->right;
+
+        while( aux != r_head && aux->column != y ) {
+	        aux = aux->right;
+        }
+        Matrix* aux_1 = aux;
+        Matrix* aux_2 = aux->below;
+        while( aux_2 != aux && aux_2->line != x ) {
+	        aux_1 = aux_2;
+	        aux_2 = aux_2->below;
+        }
+        new_node->below = aux_2;
+        aux_1->below = new_node;
+
     }
 }
-
 #endif /*MATRIX_H*/
